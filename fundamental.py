593849219b1440ef377468a1b2070561f430aad1
Paola Donis Noriega <pdonis1@jhu.edu>
2018-01-14 17:31:41 -0600

    final version

--- fundamental.py
+++ fundamental.py
@@ -1,135 +1,212 @@
 '''
 Program that calculates the fundamental matrix and draws the epipolar lines.
-It uses ORB to find the features, match_features.py to find the matches and
-compute_proj_xform to choose the right transformations and find the inliers
-using RANSAC.
+It uses ORB descriptors and finds the matches using euclidean distance.
+Then the fundamental matrix is calculated using groups of 8 matches and RANSAC
+is used to classify inliers.
 '''
 
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from match_features import match_features
-from compute_proj_xform import compute_proj_xform
 import random
+import math
+from itertools import combinations
 
 
-def detect_features(image): # function that returns a tuple with the x and y pixels where a features is located
+def detect_features(image): # function that returns keypoints and descriptors
     orb = cv2.ORB_create()
-    kp = orb.detect(image,None)
-    all_x = list()
-    all_y = list()
 
-    for keyPoint in kp:
-        x = int(keyPoint.pt[0])
-        y = int(keyPoint.pt[1])
-        all_x.append(x)
-        all_y.append(y)
-    coords = []
-    for i in range(0, 400): # use 400 of the features it returns
-        coords.append((all_y[i], all_x[i]))
-        tup = tuple(coords)
-        
-    return tup
-   
+    kp = orb.detect(image, None)
+    kp, des = orb.compute(image, kp)
+
+    return kp, des
+    
 image1 = cv2.imread('hopkins1.JPG',0) # open the images
 image1_color = cv2.imread('hopkins1.JPG')
 image_matches_1 = image1_color.copy()
-tup_1 = detect_features(image1)
+kp1, descriptors_image1 = detect_features(image1)
+all_x = list()
+all_y = list()
+    
+for keyPoint in kp1: # create a tuple with the key points
+    x = int(keyPoint.pt[0])
+    y = int(keyPoint.pt[1])
+    all_x.append(x)
+    all_y.append(y)
+coords = []
+for i in range(0, len(kp1)):
+    coords.append((all_y[i], all_x[i]))
+    tup_1 = tuple(coords)
 
-image2 = cv2.imread('hopkins2.JPG',0)
+
+image2 = cv2.imread('hopkins2.JPG',0) # open image 2 and create copies to be displayed later
 image2_color = cv2.imread('hopkins2.JPG')
 image_matches_2 = image2_color.copy()
-tup_2 = detect_features(image2)
+kp2, descriptors_image2 = detect_features(image2)
+all_x = list()
+all_y = list()
+
+for keyPoint in kp2: # create a tuple with the keypoints
+    x = int(keyPoint.pt[0])
+    y = int(keyPoint.pt[1])
+    all_x.append(x)
+    all_y.append(y)
+coords = []
+for i in range(0, len(kp2)):
+    coords.append((all_y[i], all_x[i]))
+    tup_2 = tuple(coords)
+
 
 (vertical_1, horizontal_1) = np.shape(image1)
 (vertical_2, horizontal_2) = np.shape(image2)
 
+chosen_matches = []
+thresh = 0.6 # threshold that controls the amount of matches
 
-matches = match_features(tup_1, tup_2, image1, image2) # match_features.py returns a tuple with the index of the pairs
+for index1, desc1 in enumerate(descriptors_image1): # calculate the eucledian distance of the descriptors to find the matches
+    distances = []
+    for index2, desc2 in enumerate(descriptors_image2):
+        euclidean_distance = (desc1 ^ desc2).sum()
+        distances.append(euclidean_distance)
+        
+    min_distance = min(distances)
+    next_min_dist = float(np.partition(distances, 1)[1])
+    
+    if (thresh * next_min_dist > min_distance):
+        index_neighbor = distances.index(min_distance)
+        chosen_matches.append([index1, index_neighbor])
+
+matches = tuple(chosen_matches)
+
+amount_matches = len(matches)
+
+for m in range(0, amount_matches): # graph the matches
+        tup_image_1 = tup_1[m]
+        tup_image_2 = tup_2[m]
+        cv2.circle(image_matches_1, (tup_1[matches[m][0]][1], tup_1[matches[m][0]][0]), 1, (0, 255, 0), 3)
+        cv2.circle(image_matches_2, (tup_2[matches[m][1]][1], tup_2[matches[m][1]][0]), 1, (0, 255, 0), 3)
+
+two_images_match = np.hstack((image_matches_1, image_matches_2))
+
+for m in range(0, amount_matches):
+    cv2.line(two_images_match,(tup_1[matches[m][0]][1], tup_1[matches[m][0]][0]),(tup_2[matches[m][1]][1] + horizontal_1, tup_2[matches[m][1]][0]),(255,0,0),2)
 
 
-transformation = compute_proj_xform(matches, tup_1, tup_2, image1, image2) # returns the transformation that results in more inliers
+##################### FIND ALL COMBINATIONS OF 8 MATCHES ####################################
+    
+amount_matches = len(matches)
+comb = np.arange(amount_matches)
 
-threshold = 20.0
-two_images_matches = np.hstack((image_matches_1, image_matches_2))
-correct_matches = []
+amount_combinations = math.factorial(amount_matches) / (math.factorial(8) * math.factorial(amount_matches - 8))
+all_comb = np.zeros((amount_combinations, 8))
+ranks = np.zeros((1, amount_combinations))
 
-for k in range(0, len(matches)): # using the projective transformation matrix, check if each match is an inlier (green) or outlier (blue)
+x = 0
+for p in combinations(comb, 8):
+    for q in range(0,len(p)):
+        all_comb[x][q] = int(p[q])
+    x +=1
+
+c = [] # matrix with all the matches in the format [x1, y1, x2, y2]
+
+for k in range(0, len(matches)):
     index1 = matches[k][0]
     index2 = matches[k][1]
-    x = float(tup_1[index1][1])
-    y = float(tup_1[index1][0])
-    initial_point = np.matrix([[x],[y],[1.0]])
-    x_prime = float(tup_2[index2][1])
-    y_prime = float(tup_2[index2][0])
-    calc_x = (transformation[0,0] * x + transformation[0,1] * y + transformation[0,2])/(transformation[2,0] * x + transformation[2,1] * y + transformation[2,2])
-    calc_y = (transformation[1,0] * x + transformation[1,1] * y + transformation[1,2])/(transformation[2,0] * x + transformation[2,1] * y + transformation[2,2])
-    if ((abs(calc_x - x_prime) < threshold) and (abs(calc_y - y_prime) < threshold)): # check if the match is described by the transformation
-        correct_matches.append([x,y, x_prime, y_prime])
-        cv2.circle(two_images_matches, (int(x), int(y)), 4, (255, 255, 0), 3) # graph features and matching lines for the inliers
-        cv2.circle(two_images_matches, (int(x_prime) + horizontal_1, int(y_prime)), 4, (255, 255, 0), 2)
-        cv2.line(two_images_matches,(int(x), int(y)),(int(x_prime) + horizontal_1, int(y_prime)),(255, 255, 0),2)
+    x1 = float(tup_1[index1][1])
+    y1 = float(tup_1[index1][0])
+    x2 = float(tup_2[index2][1])
+    y2 = float(tup_2[index2][0])
+    c.append([x1, y1, x2, y2])
 
-selected_points = [0, 1, 2, 8, 11, 12, 13, 14] # since many features are points nearby, selected points are points that are significantly far apart from each other
+def generate_M(selected_points): # function that generates the fundamental matrix
+    M = np.zeros((8, 9))
 
-c = correct_matches
+    # generate the M matrix to calculate the fundamental matrix
+    for i in range(0, len(selected_points)):
+        x = int(selected_points[i])
+        for y in range(0,9):
+            if (y == 0):
+                M[i][y] = c[x][0] * c[x][2]
+            elif (y == 1):
+                M[i][y] = c[x][0] * c[x][3]
+            elif (y == 2):
+                M[i][y] = c[x][0]
+            elif (y == 3):
+                M[i][y] = c[x][1] * c[x][2]
+            elif (y == 4):
+                M[i][y] = c[x][1] * c[x][3]
+            elif (y == 5):
+                M[i][y] = c[x][1]
+            elif (y == 6):
+                M[i][y] = c[x][2]
+            elif (y == 7):
+                M[i][y] = c[x][3]
+            elif (y == 8):
+                M[i][y] = 1
+            
+    U, s, V = np.linalg.svd(np.matrix(M))
 
-M = np.zeros((8, 9))
-
-# generate the M matrix to calculate the fundamental matrix
-for i in range(0, len(selected_points)):
-    x = selected_points[i]
-    for y in range(0,9):
-        if (y == 0):
-            M[i][y] = c[x][0] * c[x][2]
-        elif (y == 1):
-            M[i][y] = c[x][0] * c[x][3]
-        elif (y == 2):
-            M[i][y] = c[x][0]
-        elif (y == 3):
-            M[i][y] = c[x][1] * c[x][2]
-        elif (y == 4):
-            M[i][y] = c[x][1] * c[x][3]
-        elif (y == 5):
-            M[i][y] = c[x][1]
-        elif (y == 6):
-            M[i][y] = c[x][2]
-        elif (y == 7):
-            M[i][y] = c[x][3]
-        elif (y == 8):
-            M[i][y] = 1
-        
-U, s, V = np.linalg.svd(np.matrix(M), full_matrices=False)
-
-chosen_V = V[-1] # choose the V with the smallest singular value
+    chosen_V = V[-1] # choose the V with the smallest singular value
 
 
-(sizey, sizex) = np.shape(chosen_V)
-
-for l in range(0, sizex): # normalize V so that the last value is a 1
-        chosen_V[0,l] = chosen_V[0,l]/chosen_V[0,8]
-
-fundamental_matrix = np.reshape(chosen_V, (3, 3)) # reshape the fundamental matrix into a 3x3 matrix
-fundamental_matrix = np.matrix.transpose(fundamental_matrix) # shape the matrix the right way since V = [f11, f21, f31, f12, f22, f32, f31, f32, f33]^T
-
-
-U,s,V = np.linalg.svd(fundamental_matrix, full_matrices = False) # find the singular values to set the smallest one to 0 so that the matrix is rank 2
-
-D_prime = np.diag(s) # matrix with the singular values in its diagonal
-D_prime[2][2] = 0 #set the smallest singular value to 0
-
-F = np.dot(U, np.dot(D_prime, V)) # find the final fundamental matrix
+    (sizey, sizex) = np.shape(chosen_V)
+    
+    for l in range(0, sizex): # normalize V so that the last value is a 1
+            chosen_V[0,l] = chosen_V[0,l]/chosen_V[0,8]
+    
+    fundamental_matrix = np.reshape(chosen_V, (3, 3)) # reshape the fundamental matrix into a 3x3 matrix
+    fundamental_matrix = np.matrix.transpose(fundamental_matrix) # shape the matrix the right way since V = [f11, f21, f31, f12, f22, f32, f31, f32, f33]^T
 
 
-selected_points = [0, 1, 2, 4, 8, 11, 12, 13, 14] # chosen points to graph
+    U,s,V = np.linalg.svd(fundamental_matrix) # find the singular values to set the smallest one to 0 so that the matrix is rank 2
+
+    D_prime = np.diag(s) # matrix with the singular values in its diagonal
+    D_prime[2][2] = 0 #set the smallest singular value to 0
+
+    F = np.dot(U, np.dot(D_prime, V)) # find the final fundamental matrix
+    F= F/F[2,2]
+    
+    return F
+
+thresh_mat = 0.03 # threshold that determines which matches agree with each calculated fundamental matrix
+
+for h in range(0, amount_combinations): # iterate through all combinations
+    h = h
+    selected_points = np.zeros((8,1))
+    for p in range(0,8):
+        selected_points[p] = all_comb[h][p]
+    F = generate_M(selected_points)
+    rank = 0
+    
+    for k in range(0, amount_matches):
+        index1 = matches[k][0]
+        index2 = matches[k][1]
+        x = float(tup_1[index1][1])
+        y = float(tup_1[index1][0])
+        left = np.matrix([x,y,1.0])
+        x_prime = float(tup_2[index2][1])
+        y_prime = float(tup_2[index2][0])
+        right = np.matrix([[x_prime],[y_prime],[1.0]])
+
+        value = np.dot(np.dot(left, F), right)
+        if (value < thresh_mat): # check if each match is described by the calculated fundamental matrix
+            rank += 1
+    ranks[0][h] = rank # store the rankings to see which one agrees with more points according to RANSAC
+
+
+h = np.argmax(ranks) # chosen combination of matches
+
+selected_points = np.zeros((8,1))
+for p in range(0,8):
+    selected_points[p] = all_comb[h][p]
+F = generate_M(selected_points) # generate the matrix that will be used
+
 
 for j in range(0, len(selected_points)):
-    i = selected_points[j]
-    # find the features that will be used for the calculations of the epipolar line
+    i = int(selected_points[j])
+
     u = c[i][0]
     v = c[i][1]
-    u_point2 = c[i][2]
-    v_point2 = c[i][3]
 
     # calculate the factors a,b,c from the fundamental matrix
     a = F[0,0] * u + F[0,1] * v + F[0,2]
@@ -149,18 +226,18 @@ for j in range(0, len(selected_points)):
         v2 = (vertical_2 - 1)
         u2 = int((-c_factor-b*v2)/a)
 
+
     # generate random colors for each point
     red = random.randrange(0,255,1)
     green = random.randrange(0,255,1)
     blue = random.randrange(0,255,1)
+    
 
     cv2.line(image2_color,(u1,v1),(u2,v2),(red, green, blue),2) # graph the epipolar lines
-    cv2.circle(image1_color, (int(u),int(v)), 4, (red, green, blue), 2) # graph the points in image1
-    cv2.circle(image2_color, (int(u_point2),int(v_point2)), 4, (red, green, blue), 2) # graph the points in image2
-
+    cv2.circle(image1_color, (int(u),int(v)), 4, (red, green, blue), 2) # graph the points in image1 to which the epipolar lines correspond to
 
 combined_image = np.hstack((image1_color, image2_color)) # combine both images to be displayed next to each other
 
-cv2.imshow('matches', two_images_matches) # show the image of the matches generated and of the epipolar lines
+cv2.imshow('matches', two_images_match) # show the image of the matches generated and of the epipolar lines
 cv2.imshow('epipolar lines', combined_image)
 cv2.waitKey()
